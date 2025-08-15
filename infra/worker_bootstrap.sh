55f98128836d8ff52f55d4b1b0368aacdeae839b
Nathan Sawicki <nsawicki@umich.edu>
2025-08-15 17:46:54 -0400

    extract aws to home directory

--- infra/worker_bootstrap.sh
+++ infra/worker_bootstrap.sh
@@ -19,8 +19,8 @@ source daskbenchmarkenv/bin/activate
 # Install AWS CLI
 # -----------------------------
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+unzip awscliv2.zip -d ../../
+sudo ../../aws/install
 
 # -----------------------------
 # Install Python packages
